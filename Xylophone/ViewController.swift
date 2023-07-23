b346fc4c6a219423abe08cffd0e356d73b4e9fc0
Simon Oh <simon.oh@hey.com>
2023-07-22 19:49:05 -0500

    Initial commit

--- Xylophone/ViewController.swift
+++ Xylophone/ViewController.swift
@@ -7,15 +7,29 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    var player: AVAudioPlayer!
 
     override func viewDidLoad() {
         super.viewDidLoad()
     }
 
-
+    @IBAction func buttonPressed(_ sender: UIButton) {
+        playSound(sender.currentTitle!)
+        sender.alpha = 0.5
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.2) {
+          //Bring's sender's opacity back up to fully opaque.
+          sender.alpha = 1.0
+        }
+    }
     
-
+    func playSound(_ key: String) {
+        let url = Bundle.main.url(forResource: key, withExtension: "wav")
+        player = try! AVAudioPlayer(contentsOf: url!)
+        player?.play()
+    }
 }
 
